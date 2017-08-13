54315ecfad3acdec776be9c6eabf7642306ca212
MathewThomas358 <mathewthomas358@gmail.com>
2017-08-13 21:16:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article1', function (req, res) {
+  res.send('Article One');
+});
+
+app.get('/article2', function (req, res) {
+  res.send('Article Two');
+});
+
+
+app.get('/article3', function (req, res) {
+  res.send('Article Three');
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
